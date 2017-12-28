cf9e961c53d52cfeabc76477f8b2731218f49f1d
Yuliana <yuliana.murillo.hurtado.sa@everis.com>
2017-12-27 19:04:12 -0600

    'Cambios'

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+// Esta vendría siendo la vista
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from './empleado';
 
 @Component({
     selector: 'app-empleado',
     templateUrl: './empleado.component.html'
 })
-export class EmpleadoComponent {
+export class EmpleadoComponent implements OnInit {
   public nombre_componente = 'Componente Empleado';
-  public listado_empleados = 'Sofía, Manuel, Yuliana, Alejandro';
+  public empleado: Empleado;
+  public trabajadores: Array<Empleado>;
+  public trabajador_externo: boolean;
+  public color: string;
+  public color_seleccionado: string;
+  // public listado_empleados = 'Sofía, Manuel, Yuliana, Alejandro';
+
+  constructor() {
+    this.empleado = new Empleado('Yuliana Murillo', 17, 'Gerente', true);
+
+    this.trabajadores = [
+      new Empleado('Alejandra M.', 40, 'Enfermera', false),
+      new Empleado('AMaximiliano M.', 40, 'Ingeniero', true),
+      new Empleado('Julio M.', 40, 'Licenciado', true),
+      new Empleado('Sandra M.', 40, 'Ingeniera', true)
+    ];
+
+    this.trabajador_externo = true;
+    this.color = 'blue';
+    this.color_seleccionado = '#ccc';
+  }
+
+  ngOnInit() {
+    console.log(this.trabajadores);
+    console.log(this.empleado);
+  }
+
+  cambiarExterno(valor) {
+    this.trabajador_externo = valor;
+  }
+
+  logColorSeleccionado() {
+    console.log(this.color_seleccionado);
+  }
+
 }
--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-fruta',
     templateUrl: './fruta.component.html'
 })
-export class FrutaComponent {
+export class FrutaComponent implements OnInit {
   public nombre_componente = 'Componente Fruta';
   public listado_frutas = 'Naraja, Manzana, Pera y Sandía';
 
@@ -22,5 +22,32 @@ export class FrutaComponent {
     this.comodin = 'cualquier cosa';
     console.log(this.trabajos);
   }
+
+  ngOnInit() {
+    this.cambiarNombre();
+    this.cambiarEdad(15);
+    console.log(this.nombre + ' ' + this.edad);
+
+    // Variables y alcance
+    var uno = 8;
+    var dos = 15;
+
+    if (uno === 8 ) {
+      let uno = 3;   // let: a nivel de bloque.
+      var dos = 88;  // var: a nivel global
+
+      console.log('DENTRO DEL IF: ' + uno + ' ' + dos );
+    }
+    console.log('FUERA DEL IF: ' + uno + ' ' + dos );
+  }
+
+  cambiarNombre() {
+    this.nombre = 'Yuli';
+  }
+
+  cambiarEdad(edad) {
+    this.edad = edad;
+  }
+
 }
 
